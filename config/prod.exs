34788df670522353126424afc784a99431e29313
Joe-noh <goflb.jh@gmail.com>
2016-04-28 23:48:31 +0900

    introduce logger's file backend

--- config/prod.exs
+++ config/prod.exs
@@ -4,5 +4,17 @@ config :line_bot, LineBot.Endpoint,
   http: [port: 8000]
 
 config :logger, level: :info
+config :logger, backends: [
+  {LoggerFileBackend, :info},
+  {LoggerFileBackend, :error}
+]
+
+config :logger, :info,
+  path: "/var/log/line_bot/info.log",
+  level: :info
+
+config :logger, :error,
+  path: "/var/log/line_bot/error.log",
+  level: :error
 
 import_config "prod.secret.exs"
