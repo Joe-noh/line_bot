81c45b13f9b5cf530361ae072ceec0f695b1dfb3
Joe-noh <goflb.jh@gmail.com>
2016-04-29 00:31:06 +0900

    change log path

--- config/prod.exs
+++ config/prod.exs
@@ -3,18 +3,17 @@ use Mix.Config
 config :line_bot, LineBot.Endpoint,
   http: [port: 8000]
 
-config :logger, level: :info
 config :logger, backends: [
   {LoggerFileBackend, :info},
   {LoggerFileBackend, :error}
 ]
 
 config :logger, :info,
-  path: "/var/log/line_bot/info.log",
+  path: "logs/info.log",
   level: :info
 
 config :logger, :error,
-  path: "/var/log/line_bot/error.log",
+  path: "logs/error.log",
   level: :error
 
 import_config "prod.secret.exs"
